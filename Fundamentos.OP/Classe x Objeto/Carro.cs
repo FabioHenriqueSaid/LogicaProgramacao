91b4761b450df338f00bb7533b2f619168ed0260
fabiohenriquesaid <fabiosaid635@gmail.com>
2021-03-06 00:18:10 -0300

    Estado e Comportamento

---
+++ Fundamentos.OP/Classe x Objeto/Carro.cs
@@ -0,0 +1,16 @@
+﻿namespace Fundamentos.OOP.Classe_x_Objeto
+{
+    public class Carro
+    {
+        public string Cor { get; set; }
+        public int QuantidadePortas { get; set; }
+        public class Objeto {
+            public Objeto()
+            {
+                 Carro objeto = new Carro();
+                 objeto.Cor = "Branco";
+                 objeto.QuantidadePortas = 4;
+            }
+        }
+    }
+}
---
+++ Fundamentos.OP/Pilares/EstadoComportamento.cs
@@ -0,0 +1,13 @@
+﻿namespace Fundamentos.OOP.Pilares
+{
+    public class Carro
+    {
+        public string Modelo { get; set; }
+        public int Quilometragem { get; set; }
+
+        public void AdicionaQuilometragem()
+        {
+            Quilometragem =+ 300;
+        }
+    }
+}
